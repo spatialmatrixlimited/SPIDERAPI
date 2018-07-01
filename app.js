ed247191377b57cdd05780ff44e42e1773f8636c
Kola Grey <kolagrey@gmail.com>
2018-07-01 17:24:53 +0100

    Update by GREY

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var bodyParser = require('body-parser');
 var fs = require('fs');
 var rfs = require('rotating-file-stream');
 var path = require('path');
-var expressValidator = require('express-validator');
 var config = require('./config/database');
 var passport = require('passport');
 var routes = require('./routes/routes');
@@ -20,8 +19,7 @@ var session = require('./middleware/sessionmanager');
 var mongoose = require('mongoose');
 var cors = require('cors');
 var morgan = require('morgan');
-var analytical = require('./middleware/analytical')
-//var w3wEngine = require('./controller/w3w.engine.controller');
+var analytical = require('./middleware/analytical');
 var port = process.env.PORT || 5110;
 
 mongoose.Promise = global.Promise;
@@ -103,7 +101,6 @@ mongoose.connection.on('error', (err) => {
 // When the connection is disconnected
 mongoose.connection.on('disconnected', () => {
     console.log('Mongoose default connection disconnected');
-    clearInterval(w3wInterval);
 });
 
 // If the Node process ends, close the Mongoose connection
